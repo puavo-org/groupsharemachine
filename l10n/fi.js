ff4249d3598a8c0cb8948a7ca80a96ba84d9df4b
Tuomas Nurmi <tuomas@norsumanageri.org>
2024-05-23 13:49:34 +0300

    Add Finnish translations
    Manual collection of strings at least for now. The amount won't probably
    get that high, so maybe this is OK.

---
+++ l10n/fi.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "groupsharemachine",
+    {
+    "Share to a group" : "Jaa ryhmälle",
+    "Failed to share" : "Jakaminen epäonnistui",
+    "no matching groups found for " : "ei löytynyt täsmääviä ryhmiä ryhmälle ",
+    "Failed to search for group" : "Ryhmän etsiminen epäonnistui",
+    "Shared" : "Jaettu"
+},
+"nplurals=2; plural=(n != 1);");
